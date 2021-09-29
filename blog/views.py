3aefbb1691499f97dc0642239c5ec240e9bea2bd
jeremy.naguran <jnaguran@gmail.com>
2021-09-29 15:49:11 +0200

    project completed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+
+def blogs(request):
+    blogs = Blog.objects
+    context = {"blogs": blogs}
+    return render(request, 'blog/blogs.html', context)
+
+def details(request, blog_id):
+    blog_detial = get_object_or_404(Blog, pk=blog_id)
+    context = {"blog_detail": blog_detial}
+    return render(request, 'blog/detail.html', context)
+
+
