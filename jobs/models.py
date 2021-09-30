d97924549e92c29c5ded330e23a9e2ec27c4a53b
jeremy.naguran <jnaguran@gmail.com>
2021-09-30 09:57:08 +0200

    ameneded jobs table

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
+    job_name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.job_name
